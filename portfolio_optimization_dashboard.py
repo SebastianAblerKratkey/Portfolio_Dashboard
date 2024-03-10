8727fb765fcb2ee563ddb090691036b5f38ff28e
SebastianAblerKratkey <124098334+SebastianAblerKratkey@users.noreply.github.com>
2024-03-10 01:47:43 +0100

    Update portfolio_optimization_dashboard.py

--- portfolio_optimization_dashboard.py
+++ portfolio_optimization_dashboard.py
@@ -867,7 +867,7 @@ if download_sucess:
         mvf_plot_data["std"] = min_var_list
 
         #txt_ = "Backtest"
-        st.write(f"**{"Minimum varriance frontier and capital allocation line"}**")
+        st.write(f"**{str(Minimum varriance frontier and capital allocation line)}**")
         create_mvf_cal_visual()
         st.pyplot()
 
