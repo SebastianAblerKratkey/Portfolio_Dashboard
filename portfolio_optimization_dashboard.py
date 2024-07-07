6a9b3d45e56ba470cb12cb3844d4f6880de03a54
SebastianAblerKratkey <124098334+SebastianAblerKratkey@users.noreply.github.com>
2024-07-07 13:43:52 +0200

    Update portfolio_optimization_dashboard.py

--- portfolio_optimization_dashboard.py
+++ portfolio_optimization_dashboard.py
@@ -1908,11 +1908,20 @@ if download_sucess:
         breakeven_at_exiry = strike_price + call_price_at_purchase/subscription_ratio
 
         black_scholes_value = black_scholes_call_value(S0=spot_price, K=strike_price, rf=rf, T=time_in_years, vol=impl_vol) * subscription_ratio
+        call_option_leverage = call_lambda(S0=spot_price, K=strike_price, rf=rf, T=time_in_years, vol=impl_vol)
+        
+        # Row 1
         colmn_1, colmn_2, colmn_3, colmn_4 = st.columns([0.5, 0.5, 0.5, 0.5]) 
         colmn_1.metric("Black-Scholes option value", f"{black_scholes_value:.2f}")
         colmn_2.metric("Spot price underlying", f"{spot_price:.2f}")
         colmn_3.metric("Breakeven price at exiry", f"{breakeven_at_exiry:.2f}")
         colmn_4.metric("Trading days to expiration", f"{number_trading_days:.0f}")
+        # Row 2
+        colmn_5, colmn_6, colmn_7, colmn_8 = st.columns([0.5, 0.5, 0.5, 0.5]) 
+        colmn_5.metric("Leverage", f"{call_option_leverage:.2f}")
+        colmn_6.metric("Spot price underlying", f"{spot_price:.2f}")
+        colmn_7.metric("Breakeven price at exiry", f"{breakeven_at_exiry:.2f}")
+        colmn_8.metric("Trading days to expiration", f"{number_trading_days:.0f}")
 
         headline1 = "Distribution fitting"
         st.write(f"**{headline1}**")
