a91ac34b735bb7894503323dcd6ab53519ab113d
SebastianAblerKratkey <124098334+SebastianAblerKratkey@users.noreply.github.com>
2024-07-19 22:01:14 +0200

    Update portfolio_optimization_dashboard.py

--- portfolio_optimization_dashboard.py
+++ portfolio_optimization_dashboard.py
@@ -1925,9 +1925,9 @@ if download_sucess:
         #subscription ratio
         subscription_ratio = st.number_input("Subscription ratio (how many units of the underlying the option refers to)", value=0.01)
 
-        default_impl_vol = return_data.std() * (trading_days_per_year)**0.5
+        #default_impl_vol = return_data.std() * (trading_days_per_year)**0.5
         #default_black_scholes_value = black_scholes_call_value(S0=spot_price, K=strike_price, rf=rf, T=time_in_years, vol=default_impl_vol) * subscription_ratio
-        black_scholes_value = st.number_input("Call opiton price – default assumes volatility equalt to annualized std of daily log returns", value=black_scholes_call_value(S0=spot_price, K=strike_price, rf=rf, T=time_in_years, vol=default_impl_vol) * subscription_ratio)
+        black_scholes_value = st.number_input("Call opiton price – default assumes volatility equalt to annualized std of daily log returns", value=black_scholes_call_value(S0=spot_price, K=strike_price, rf=rf, T=time_in_years, vol=return_data.std() * (trading_days_per_year)**0.5) * subscription_ratio)
 
         adj_price = black_scholes_value * (1/subscription_ratio)
         default_impl_vol = call_implied_volatility(S0=spot_price, K=strike_price, rf=rf, T=time_in_years, call_option_market_price = adj_price, a=-2.0, b=2.0, xtol=1e-6)
