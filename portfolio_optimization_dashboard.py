3b6ca7e7951925541fff6b1b3f0577840c07e588
SebastianAblerKratkey <124098334+SebastianAblerKratkey@users.noreply.github.com>
2024-12-10 03:29:59 +0100

    Update portfolio_optimization_dashboard.py

--- portfolio_optimization_dashboard.py
+++ portfolio_optimization_dashboard.py
@@ -1973,6 +1973,7 @@ if download_sucess:
         
         rolling_window = st.number_input("Input number of days for rolling volatility calculation", value=30)
         vol_start_date = st.date_input("Set starting date for long-run volatilty calculation", value=price_data.index.min(), max_value= current_date - timedelta(days=3*365), min_value=price_data.index.min(), format="DD.MM.YYYY")
+        vol_start_date = pd.Timestamp(vol_start_date)
         lookback_years = st.number_input("How many previous years shall be displayed?", value=5)
 
         # Create vol_data as a copy of price_data
